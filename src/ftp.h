c1ad1b7c7bea14e7c74174671c79d4fb25078796
Kurt Kanzenbach <kurt@kmk-computers.de>
2016-10-12 18:45:14 +0200

    main: added missing license headers

--- src/ftp.h
+++ src/ftp.h
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2015-2016 Kurt Kanzenbach <kurt@kmk-computers.de>
+ *
+ * This file is part of Get.
+ *
+ * Get is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Get is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Get.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef _FTP_H_
 #define _FTP_H_
 
