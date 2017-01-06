2c46c71b5433bebd166bebb09d545fa6a025e0fc
Kurt Kanzenbach <kurt@kmk-computers.de>
2017-01-06 12:57:46 +0100

    dir-locals: set cmake-ide-build-dir for cmake-ide mode

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,7 @@
 ;; Coding style
 ((nil . ((indent-tabs-mode . nil)
          (fill-column . 80)
-         (tab-width . 4)))
+         (tab-width . 4)
+         (cmake-ide-build-dir . "build")))
  (c-mode . ((c-file-style . "linux")
             (c-basic-offset . 4))))
