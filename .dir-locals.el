0116f4487ff71492df22f721de5588aea8bfc8d2
Kurt Kanzenbach <kurt@kmk-computers.de>
2017-09-25 19:44:40 +0200

    dir-locals: removing build dir
    Signed-off-by: Kurt Kanzenbach <kurt@kmk-computers.de>

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,6 @@
 ;; Coding style
 ((nil . ((indent-tabs-mode . nil)
          (fill-column . 80)
-         (tab-width . 4)
-         (cmake-ide-build-dir . "build")))
+         (tab-width . 4)))
  (c-mode . ((c-file-style . "linux")
             (c-basic-offset . 4))))
